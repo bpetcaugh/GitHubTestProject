2e758b5434bfde53e28b587c06d2b008b0f03454
Brandon Petcaugh <bpetcaugh@gmail.com>
2021-11-19 10:32:09 -0500

    Added welcome function

--- src/Tester1.java
+++ src/Tester1.java
@@ -2,4 +2,8 @@ public class Tester1 {
     public static void main(String[] args) {
         System.out.println("Testing 1 2 1 2...");
     }
+
+    static public void welcome() {
+        System.out.println("Welcome");
+    }
 }
